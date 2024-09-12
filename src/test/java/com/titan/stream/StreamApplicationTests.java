e5256a6193be69bbaf35071785468711799aef8c
imran <imran>
2024-09-12 18:09:26 +0530

    New project

---
+++ src/test/java/com/titan/stream/StreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.titan.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
